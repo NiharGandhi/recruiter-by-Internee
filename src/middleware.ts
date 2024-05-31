d613554798783841a79d3afc33fdd7f31c87b00f
Nihar Gandhi <nihargandhi93@gmail.com>
2024-05-31 19:13:44 +0400

    Middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@ const isProtectedRoute = createRouteMatcher([
     '/admin(.*)',
     '/dashboard(.*)',
     '/myInternships(.*)',
+    '/myOrganiation(.*)',
     '/createInternship(.*)',
     '/users(.*)',
 ]);
