a845b67cdb9d4ed83a7f1c18c6bd5f0693fe7d85
Nihar Gandhi <nihargandhi93@gmail.com>
2024-06-05 14:06:07 +0400

    Header Change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Internee",
+  title: "Internee Recruiter",
   description: "The One App For All your Internship needs.",
 };
 
