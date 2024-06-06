51d4bb3607e34aed1105bc5949c5ed9d5cceb2df
Nihar Gandhi <nihargandhi93@gmail.com>
2024-06-06 14:47:42 +0400

    Header Fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,17 +4,30 @@ import React from 'react'
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
 import ShimmerButton from './magicui/shimmer-button'
+import { auth } from '@clerk/nextjs/dist/types/server'
 
 const Header = () => {
+
+    const { userId } = auth();
+
     return (
         <div className='bg-[#6c5ce7] dark:border-gray-800 dark:bg-gray-950 flex items-center justify-between py-2 px-4'>
             <div className='flex flex-col lg:flex-row items-start lg:items-center'>
-                <Link href="/">
-                    <div className='flex flex-col lg:flex-row lg:items-baseline'>
-                        <h1 className='font-bold text-white text-3xl font-sans'>Internee</h1>
-                        <h1 className='text-white font-sans text-2xl lg:ml-2'>Recruiter</h1>
-                    </div>
-                </Link>
+                {userId ? (
+                    <Link href="/dashboard">
+                        <div className='flex flex-col lg:flex-row lg:items-baseline'>
+                            <h1 className='font-bold text-white text-3xl font-sans'>Internee</h1>
+                            <h1 className='text-white font-sans text-2xl lg:ml-2'>Recruiter</h1>
+                        </div>
+                    </Link>
+                ) : (
+                    <Link href="/">
+                        <div className='flex flex-col lg:flex-row lg:items-baseline'>
+                            <h1 className='font-bold text-white text-3xl font-sans'>Internee</h1>
+                            <h1 className='text-white font-sans text-2xl lg:ml-2'>Recruiter</h1>
+                        </div>
+                    </Link>
+                )}
             </div>
             <div className='flex items-center ml-auto'>
                 <SignedOut>
